1147ef565cca79632077c5443d205a1ce4ea75ac
Gastonite <matthew.r.gaston@gmail.com>
2021-11-02 20:07:53 +0000

    test

--- Bot.py
+++ Bot.py
@@ -2,8 +2,8 @@ from adafruit_motorkit import MotorKit
 kit = MotorKit()
 import time
 
-kit.motor1.throttle = 0.5
-kit.motor4.throttle = 0.5
+kit.motor1.throttle = 0.3
+kit.motor4.throttle = 0.3
 time.sleep(1)
 kit.motor1.throttle = 0
-kit.motor4.throttle = 0.5
+kit.motor4.throttle = 0
