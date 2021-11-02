77ff02aee5e7f98e6523b73f3d6e41d1a15eab7d
Gastonite <matthew.r.gaston@gmail.com>
2021-11-02 20:09:37 +0000

    test

--- Bot.py
+++ Bot.py
@@ -5,5 +5,8 @@ import time
 kit.motor1.throttle = 0.3
 kit.motor4.throttle = -0.3
 time.sleep(1)
+kit.motor1.throttle = -0.3
+kit.motor4.throttle = 0.3
+time.sleep(1)
 kit.motor1.throttle = 0
 kit.motor4.throttle = 0
