4f87a757e041a304cee5ef194d81381c36d63a75
Gastonite <matthew.r.gaston@gmail.com>
2021-11-14 12:46:19 +0000

    test

--- Servo.py
+++ Servo.py
@@ -6,8 +6,8 @@ import Adafruit_PCA9685
 pwm0 = Adafruit_PCA9685.PCA9685(address=0x60)
 
 # Configure min and max servo pulse lengths
-servo_min = 300  # Min pulse length out of 4096
-servo_max = 550  # Max pulse length out of 4096
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 450  # Max pulse length out of 4096
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(pwmno,channel, pulse):
