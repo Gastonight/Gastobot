b832b7993b768cb5d975a1b90a4e66277d408db2
Gastonite <matthew.r.gaston@gmail.com>
2021-11-14 15:37:24 +0000

    test

--- Servo.py
+++ Servo.py
@@ -1,13 +1,16 @@
 from __future__ import division
 import time
 
+from approxeng.input.selectbinder import ControllerResource
+
 import Adafruit_PCA9685
 
 pwm0 = Adafruit_PCA9685.PCA9685(address=0x60)
 
 # Configure min and max servo pulse lengths
 servo_min = 120  # Min pulse length out of 4096
-servo_max = 300  # Max pulse length out of 4096
+servo_max = 280  # Max pulse length out of 4096
+servo_start = 200
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(pwmno,channel, pulse):
@@ -26,10 +29,55 @@ def set_servo_pulse(pwmno,channel, pulse):
 # Set frequency to 60hz, good for servos.
 pwm0.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
-    # Move servo on board 0 channel O between extremes.
-    pwm0.set_pwm(15, 0, servo_min)
-    time.sleep(5)
-    pwm0.set_pwm(15, 0, servo_max)
-    time.sleep(5)
+#print('Moving servo on channel 0, press Ctrl-C to quit...')
+#while True:
+#    # Move servo on board 0 channel O between extremes.
+#    pwm0.set_pwm(15, 0, servo_min)
+#    time.sleep(5)
+#    pwm0.set_pwm(15, 0, servo_max)
+#    time.sleep(5)
+
+pwm0.set_pwm(15, 0, servo_start)
+
+try:
+    while True:
+        try:
+            with ControllerResource(dead_zone=0.1, hot_zone=0.2) as joystick:
+                print('Controller found, press HOME button to exit, use left stick to drive.')
+                print(joystick.controls)
+                while joystick.connected:
+                    x_axis, y_axis, servo_axis = joystick['rx', 'ry', 'lx']
+                    servo_start = servo_start + servo_axis
+                    pwm0.set_pwm(15, 0, servo_start)
+                    #power_left, power_right = mixer(yaw=x_axis, throttle=y_axis)
+                    #set_speeds(-power_left/100, power_right/100)
+                    # Get a ButtonPresses object containing everything that was pressed since the last
+                    # time around this loop.
+                    joystick.check_presses()
+                    # Print out any buttons that were pressed, if we had any
+                    if joystick.has_presses:
+                    # If home was pressed, raise a RobotStopException to bail out of the loop
+                    # Home is generally the PS button for playstation controllers, XBox for XBox etc
+                        if 'home' in joystick.presses:
+                            raise RobotStopException()
+                        if 'ddown' in joystick.presses:
+                            if max_power > 50:
+                                max_power = max_power - 10
+                                print("Set to", max_power, "% speed")
+                            else:
+                                print("Already minimum speed")
+                        if 'dup' in joystick.presses:
+                            if max_power < 100:
+                                max_power = max_power + 10
+                                print("Set to", max_power, "% speed")
+                            else:
+                                print("Already maximum speed")
+        except IOError:
+            # We get an IOError when using the ControllerResource if we don't have a controller yet,
+            # so in this case we just wait a second and try again after printing a message.
+            print('No controller found yet')
+            sleep(1)
+except RobotStopException:
+    # This exception will be raised when the home button is pressed, at which point we should
+    # stop the motors.
+    stop_motors()
