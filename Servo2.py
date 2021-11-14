a772584b9607a3cc46601d0552e540a9c3ef72ef
Gastonite <matthew.r.gaston@gmail.com>
2021-11-14 13:49:47 +0000

    test

--- Servo2.py
+++ Servo2.py
@@ -1,7 +1,8 @@
 from adafruit_servokit import ServoKit
-kit = ServoKit(channels=16)
+#kit = ServoKit(channels=16)
 from time import sleep
 
 kit = PWM(0x60)
+kit = ServoKit(channels=16)
 
 kit.servo[15].angle = 90
